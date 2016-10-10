ca2d0d72c9eba508927b07af566e030798c1ed02
Tomas Valkonen <tomas.valkonen@gmail.com>
2016-10-10 11:53:52 +0300

    Touch testing

--- MobileMarbleJam/Assets/Scripts/TouchControl.cs
+++ MobileMarbleJam/Assets/Scripts/TouchControl.cs
@@ -68,97 +68,34 @@ public class TouchControl : MonoBehaviour {
 			for (int i = 0; i < Input.touchCount; i++) {	
 				Touch currentTouch = Input.GetTouch (i);
 
-				if (currentTouch.phase == TouchPhase.Began) {
-					Vector2 v2 = new Vector2 (Camera.main.ScreenToWorldPoint (currentTouch.position).x, Camera.main.ScreenToWorldPoint (currentTouch.position).y);
-					Collider2D hit = Physics2D.OverlapPoint (v2, layerMask);
+                Vector2 v2 = new Vector2(Camera.main.ScreenToWorldPoint(currentTouch.position).x, Camera.main.ScreenToWorldPoint(currentTouch.position).y);
+                Collider2D hit = Physics2D.OverlapPoint(v2, layerMask);
 
-					if (hit != null && hit.GetComponent<ButtonTile> () != null) {
-						hit.GetComponent<ButtonTile> ().TouchButton ();
-					}
-					if (hit != null && hit.GetComponent<StartGameButton> () != null) {
-						hit.GetComponent<StartGameButton> ().TouchButton ();
-					}
-                
+                if (currentTouch.phase == TouchPhase.Began)
+                {
 
-					// jos osutaan johonkin muuhun kuin nappiin tai reunaan
-					if (hit != null && hit.GetComponent<ButtonTile> () == null && GetComponent<StartGameButton> () == null && hit.gameObject.tag != "Boundary") {
-						//Destroy(hit.gameObject);
-						createTile.CreateTileOnPosition (hit);
-					}
+                    if (hit != null && hit.GetComponent<ButtonTile>() != null)
+                    {
+                        hit.GetComponent<ButtonTile>().TouchButton();
+                    }
+                    else if (hit != null && hit.GetComponent<StartGameButton>() != null)
+                    {
+                        hit.GetComponent<StartGameButton>().TouchButton();
+                    }
+                }
+                if (currentTouch.phase == TouchPhase.Moved || currentTouch.phase == TouchPhase.Began)
+                {
+                    if (hit != null && hit.GetComponent<ButtonTile>() == null && GetComponent<StartGameButton>() == null && hit.gameObject.tag != "Boundary")
+                    {
+                        //Destroy(hit.gameObject);
+                        createTile.CreateTileOnPosition(hit);
+                    }
+                }
 					
-				}
-			}
-
-		}
-	/*
-        //Touch control
-
-
-		if (Input.touchCount > 0) {
-			Touch touch = Input.GetTouch (0);
-
-			switch (touch.phase) {
-			//alku kosketus
-			case TouchPhase.Began:
-				startPos = touch.position;
-				directionChosen = false;
-				//target = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-				hit = Physics2D.OverlapPoint(touch.position);
-				break;
-			// liikutus
-			case TouchPhase.Moved:
-				direction = touch.position - startPos;
-				directionChosen = false;
-				hit = Physics2D.OverlapPoint(touch.position);
-				break;	
-			//irrotus
-			case TouchPhase.Ended:
-				directionChosen = true;
-				break;
-			}
-			if (hit != null && hit.GetComponent<ButtonTile>() != null)
-			{
-				hit.GetComponent<ButtonTile>().TouchButton();
-			}
-			// jos osutaan johonkin muuhun kuin nappiin tai reunaan
-			else if (hit != null && hit.GetComponent<ButtonTile>() == null && hit.gameObject.tag != "Boundary")
-			{
-				//if (selectionManager.GetButtonTile() != null)
-				//{
-					Destroy(hit.gameObject);
-					createTile.CreateTileOnPosition(hit.transform.position);
-				//}
-				//else if (selectionManager.GetButtonTile() == null)
-				//{
-				//	Destroy(hit.gameObject);
-				//	createTile.CreateTileOnPosition(hit.transform.position);
-				//}
 			}
 
 		}
 
-			if ( Input.GetTouch(0).phase == TouchPhase.Began)
-            target = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-            //position = target;
-            //Move(position);
-            hit = Physics2D.OverlapPoint(target);
-
-            //jos osutaan nappiin
-            
-        }
-
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            target = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-            position = target;
-            Move(position);
-            hit = Physics2D.OverlapPoint(transform.position);
-            if (hit != null && Input.touchCount > 0 && hit.GetComponent<ButtonTile>() != null)
-            {
-                hit.GetComponent<ButtonTile>().TouchButton();
-            }
-        }
-        */
 
     }
         
